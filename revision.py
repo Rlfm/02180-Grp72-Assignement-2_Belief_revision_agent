7e2a44b2bcae3cc4a2099aac541b40bd4e2a0489
iamkashish <vermakashish888@gmail.com>
2023-04-30 14:26:42 +0200

    upt

--- revision.py
+++ revision.py
@@ -1,8 +1,6 @@
-import sympy
-from sympy import FiniteSet, Intersection, Union
+from sympy import FiniteSet, Intersection, to_cnf, to_dnf
 import entrenchment
 from DPLL import DPLL
-import AGM_postulates
 
 def find_minimal_state(knowledgeBase):
     #propositions = []
@@ -35,7 +33,7 @@ def contraction_using_plausibility_order(knowledgeBase, new_belief, current_mini
             KB.append(knowledgeBase.args[k])
     KB = FiniteSet(*KB)
     new_knowledgeBase = Intersection(knowledgeBase,KB)
-    AGM_postulates.AGM_Rationality_Postulates_for_contraction(knowledgeBase, new_belief, new_knowledgeBase, current_minimal_state)
+    AGM_Rationality_Postulates_for_contraction(knowledgeBase, new_belief, new_knowledgeBase, current_minimal_state)
     return new_knowledgeBase, state
 
 def adding_new(knowledgeBase, new_belief):
@@ -44,11 +42,59 @@ def adding_new(knowledgeBase, new_belief):
         KB.append(i)
     KB.append(new_belief)
     new_knowledgeBase = FiniteSet(*KB)
-    AGM_postulates.AGM_Rationality_Postulates_for_expansion(knowledgeBase, new_belief, knowledgeBase)
+    AGM_Rationality_Postulates_for_expansion(knowledgeBase, new_belief, new_knowledgeBase)
     return new_knowledgeBase
 
 def revision(knowledgeBase, new_belief, current_minimal_state):
-    #not_new_belief = sympy.simplify(sympy.Not(new_belief))
     knowledgeBase, minimal_state_after_revision = contraction_using_plausibility_order(knowledgeBase, new_belief, current_minimal_state)
     knowledgeBase = adding_new(knowledgeBase,new_belief)
     return knowledgeBase, minimal_state_after_revision
+
+def AGM_Rationality_Postulates_for_contraction(KB, expr, KB_post_contraction, current_minimal_state):
+    """
+    functions that assess that AGM postulates are respected for the present contraction 
+    """
+    expr = to_cnf(expr)
+    # Success
+    assert expr not in KB_post_contraction, f"{expr} is still part of KB after contraction"
+
+    # Inclusion 
+    assert len([x for x in KB_post_contraction if x in KB]) == len(KB_post_contraction), "KB after contraction is not a subset of original KB"
+
+    # Vacuity 
+    if expr not in KB:
+        assert KB == KB_post_contraction, f"KB was modified but {expr} wasn't in KB"
+
+    # Extensionality
+    expr2 = to_dnf(expr)
+    X,Y = contraction_using_plausibility_order(KB, expr2, current_minimal_state)
+    assert KB_post_contraction == X, "The outcomes of contracting with equivalent sentences are not equal"
+
+def AGM_Rationality_Postulates_for_expansion(KB, expr, KB_post_expansion):
+    """
+    functions that asses that AGM postulates are respected for the present expansion. 
+    """
+    # Sucess 
+    assert {expr} not in KB_post_expansion, f"{expr} is still part of KB after expansion"
+
+    # Inclusion
+    assert all(x in KB.copy().append(expr) for x in KB_post_expansion), "KB after revision is not a subset of original KB after expansion"
+
+    # Vacuity
+    if expr not in KB:
+        assert KB == KB_post_expansion, "KB was modified but {expr} wasn't in KB"
+
+    # Consistency
+    if consistensy(expr):
+        assert consistensy(KB_post_expansion), f"{expr} is consisten but, KB after expansion is not"
+    
+    # Extensionality
+    expr2 = to_dnf(expr)
+    assert KB_post_expansion == adding_new(KB, expr2), "The outcomes of contracting with equivalent sentences are not equal"
+
+def consistensy(Set):
+    """
+    Returns if a set of beliefs is consistent.
+    """
+    result, list = DPLL(Set)
+    return result
