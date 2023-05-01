0c92978d1f3c07f14bbf8b856a2fd6323ce94667
iamkashish <vermakashish888@gmail.com>
2023-05-01 16:17:41 +0200

    Another update

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from sympy import FiniteSet
 from DPLL import DPLL
 import revision
 
-# Define some symbols
+# Define sympy symbols
 A, B, C, D, E, F = sympy.symbols('A B C D E F')
 
+# Belief statements
 expr1 = sympy.Implies(A,B)
 expr2 = sympy.Not(B)
 expr3 = A
@@ -14,30 +15,33 @@ expr5 = B
 expr6 = C
 expr7 = sympy.Not(sympy.Or(C, A))
 
+# New belief which needs to be added to the agent beliefs
 add_expr_1 = sympy.to_cnf(expr7)
 
+# Initializing knowledge base of the agent and minimal state
 pre_KB = [expr3, expr5, expr6]
 KB = {sympy.to_cnf(expr) for expr in pre_KB}
-new_KB = FiniteSet(*KB)
-symbols = new_KB.free_symbols
-
-current_minimal_state = revision.find_minimal_state(new_KB)
+agent_KB = FiniteSet(*KB)
+symbols = agent_KB.free_symbols
+current_minimal_state = revision.find_minimal_state(agent_KB)
 print("Before revision")
-print(new_KB)
+print(agent_KB)
 print(current_minimal_state)
+
+# The below line of code performs revision
 new_belief = add_expr_1
 symbols = symbols | new_belief.free_symbols
 symbols = [i for i in symbols]
-KB_1 = revision.adding_new(new_KB, new_belief)
+KB_1 = revision.adding_new(agent_KB, new_belief)
 test_KB = {k for k in KB_1}
 check_inconsistency = DPLL(test_KB,symbols)
-if check_inconsistency[0]==False:
-    KB_1, current_minimal_state = revision.revision(new_KB, new_belief, current_minimal_state)
+if check_inconsistency[0]==False:                                       # If the coming belief is NOT consistent with the agent beliefs
+    KB_1, current_minimal_state = revision.revision(agent_KB, new_belief, current_minimal_state)
     print("After revision")
     print(KB_1)
     print(current_minimal_state)
-else:
-    KB_1 = revision.adding_new(new_KB, new_belief)
+else:                                                                   # If the coming belief is consistent with the agent beliefs
+    KB_1 = revision.adding_new(agent_KB, new_belief)
     current_minimal_state = revision.find_minimal_state(KB_1)
     print("After revision")
     print(KB_1)
